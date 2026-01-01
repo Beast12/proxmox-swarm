69d9a4c67d93fb1b6f439bb21d3421ace5848eab
Beast <vanoppen.koen@gmail.com>
2026-01-01 11:26:41 +0100

    Refactor VM naming conventions and enhance Docker installation in cloud-init scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "swarm_manager_vms" {
   for_each = var.swarm_managers
   source   = "./modules/proxmox-vm"
 
-  name                = "${local.name_pfx}-swarm-${each.key}"
+  name                = "swarm-manager-${each.key}"
   vmid                = each.value.vmid
   node_name           = each.value.node
   datastore           = var.default_vm_storage
@@ -20,6 +20,7 @@ module "swarm_manager_vms" {
   dns     = var.lan_dns
 
   ssh_public_keys = var.ssh_public_keys
+  password_hash   = var.vm_password_hash
   tags            = concat(local.tags_common, ["role-swarm-manager"])
 }
 
@@ -27,7 +28,7 @@ module "swarm_worker_vms" {
   for_each = var.swarm_workers
   source   = "./modules/proxmox-vm"
 
-  name                = "${local.name_pfx}-swarm-${each.key}"
+  name                = "swarm-worker-${each.key}"
   vmid                = each.value.vmid
   node_name           = each.value.node
   datastore           = var.default_vm_storage
@@ -44,6 +45,7 @@ module "swarm_worker_vms" {
   dns     = var.lan_dns
 
   ssh_public_keys = var.ssh_public_keys
+  password_hash   = var.vm_password_hash
   tags            = concat(local.tags_common, ["role-swarm-worker"])
 }
 
